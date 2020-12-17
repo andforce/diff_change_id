ffaeefd4dc564411bba5ebd71d9e4c26afbc0fa5
andforce <86118@163.com>
2020-12-17 18:29:00 +0800

    add comment

--- diff.sh
+++ diff.sh
@@ -13,10 +13,14 @@ then
 fi
 
 parojet_path=$(pwd)
-echo "${parojet_path}"
+#echo "${parojet_path}"
 
 git --no-pager log "$1"| grep "Change-Id:" | cut -d ' ' -f 6 > "${parojet_path}"/"$1".gitlog
 git --no-pager log "$2"| grep "Change-Id:" | cut -d ' ' -f 6 > "${parojet_path}"/"$2".gitlog
-echo diff branch "$1".."$2"
+echo "|=========================================================|"
+echo "| diff  branch  $1..$2"
+echo "| <:  $1  -YES" " $2  -NO"
+echo "| >:  $1  -NO"  " $2  -YES"
+echo "|=========================================================|"
 diff "${parojet_path}"/"$1".gitlog "${parojet_path}"/"$2".gitlog
 rm -f "${parojet_path}"/"$1".gitlog "${parojet_path}"/"$2".gitlog
