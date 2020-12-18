10af6e8db0361cce2c10b02cf24d9e99d1bf94c7
andforce <86118@163.com>
2020-12-18 19:18:23 +0800

    refactor code

--- main.py
+++ main.py
@@ -11,26 +11,22 @@ COMMIT_MESSAGE = 'message'
 COMMIT_CHANGE_ID = 'change_id'
 
 
-def _print_red_log(message):
-    print("\033[0;31m%s\033[0m" % message)
+def _red_log(message):
+    return "\033[0;31m%s\033[0m" % message
 
 
-def _print_green_log(message):
-    print("\033[0;32m%s\033[0m" % message)
+def _green_log(message):
+    return "\033[0;32m%s\033[0m" % message
 
 
-def _print_yellow_log(message):
-    print("\033[0;33m%s\033[0m" % message)
-
-
-def _print_normal_log(message):
-    print(message)
+def _yellow_log(message):
+    return "\033[0;33m%s\033[0m" % message
 
 
 def git_log(branch_name):
     git_commit_fields = [COMMIT_ID, COMMIT_AUTHOR_NAME, COMMIT_AUTHOR_EMAIL, COMMIT_DATE, COMMIT_MESSAGE, COMMIT_BODY]
-    git_log_command = 'git --no-pager log ' + branch_name + ' --format="%H%x1f%an%x1f%ae%x1f%ad%x1f%s%x1f%b%x1e"'
-    print(git_log_command)
+    git_log_command = 'git --no-pager log ' + branch_name + ' --format="%H%x1f%an%x1f%ae%x1f%ad%x1f%s%x1f%b%x1e"' + " --date=format:'%Y/%m/%d-%H:%M:%S'"
+    # print(git_log_command)
     p = subprocess.Popen(git_log_command, shell=True, stdout=subprocess.PIPE)
     (logs, _) = p.communicate()
     logs = logs.decode(encoding='UTF-8')
@@ -51,9 +47,6 @@ def git_log(branch_name):
                 one_log[COMMIT_CHANGE_ID] = "NOT_FIND"
         else:
             one_log[COMMIT_CHANGE_ID] = "NONE"
-
-    # for row in log:
-    #     print(row[COMMIT_ID] + " - " + row.get(COMMIT_CHANGE_ID))
     return logs
 
 
@@ -68,17 +61,44 @@ changeId2 = []
 for row in log2:
     if row.get(COMMIT_CHANGE_ID) != 'NOT_FIND':
         changeId2.append(row.get(COMMIT_CHANGE_ID))
-print(len(changeId2))
 
+print("branch " + _yellow_log(sys.argv[1]) + " have " + str(len(log1)) + " commits, " + "branch " + _yellow_log(
+    sys.argv[2]) + " have " + str(len(log2)) + " commits.")
+
+# IN changeId1, BUT NOT IN changeId2
 diff_list = list(set(changeId1).difference(set(changeId2)))
+# resort by git log
+diff_list.sort(key=changeId1.index)
+if len(diff_list) == 0:
+    print(_green_log('All patches in branch ') + _yellow_log(sys.argv[1]) + _green_log(
+        ' are already in branch ') + _yellow_log(sys.argv[2]))
+else:
+    if len(diff_list) == 1:
+        print(_red_log("there is ") + str(len(diff_list)) + _red_log(" patch that belong ") + _yellow_log(
+            sys.argv[1]) + _red_log(" is NOT in ") + _yellow_log(sys.argv[2]))
+    else:
+        print(_red_log("there are ") + str(len(diff_list)) + _red_log(" patches that belong ") + _yellow_log(
+            sys.argv[1]) + _red_log(" are NOT in ") + _yellow_log(sys.argv[2]))
+    for diff in diff_list:
+        for row in log1:
+            if row.get(COMMIT_CHANGE_ID) == diff:
+                print(_yellow_log(row[COMMIT_ID]) + " " + row[COMMIT_DATE] + " " + row[COMMIT_MESSAGE])
 
-print("\033[0;33m%s\033[0m" % "输出红色字符")
-print("                               commit id" + " - " + "changeId                        ")
-for diff in diff_list:
-    for row in log1:
-        if row.get(COMMIT_CHANGE_ID) == diff:
-            # print(row[COMMIT_ID] + " - " + row[COMMIT_CHANGE_ID])
-            _print_yellow_log(row[COMMIT_ID] + " - " + row[COMMIT_CHANGE_ID])
-
-
-
+# IN changeId2, BUT NOT IN changeId2
+diff_list = list(set(changeId2).difference(set(changeId1)))
+# resort by git log
+diff_list.sort(key=changeId2.index)
+if len(diff_list) == 0:
+    print(_green_log('All patches in branch ') + _yellow_log(sys.argv[2]) + _green_log(
+        ' are already in branch ') + _yellow_log(sys.argv[1]))
+else:
+    if len(diff_list) == 1:
+        print(_red_log("there is ") + str(len(diff_list)) + _red_log(" patch that belong ") + _yellow_log(
+            sys.argv[2]) + _red_log(" is NOT in ") + _yellow_log(sys.argv[1]))
+    else:
+        print(_red_log("there are ") + str(len(diff_list)) + _red_log(" patches that belong ") + _yellow_log(
+            sys.argv[2]) + _red_log(" are NOT in ") + _yellow_log(sys.argv[1]))
+    for diff in diff_list:
+        for row in log2:
+            if row.get(COMMIT_CHANGE_ID) == diff:
+                print(_yellow_log(row[COMMIT_ID]) + " " + row[COMMIT_DATE] + " " + row[COMMIT_MESSAGE])
