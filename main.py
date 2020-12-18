4b29d8edf5c7b3bc1686217c5738a72242a6fb2a
andforce <86118@163.com>
2020-12-18 15:25:45 +0800

    refactor code

--- main.py
+++ main.py
@@ -2,54 +2,56 @@
 import subprocess
 import sys
 
-def git_log(str):
-    GIT_COMMIT_FIELDS = ['id', 'author_name', 'author_email', 'date', 'message', 'body']
-    GIT_LOG_FORMAT = ['%H', '%an', '%ae', '%ad', '%s', '%b']
-    GIT_LOG_FORMAT = '%x1f'.join(GIT_LOG_FORMAT) + '%x1e'
+COMMIT_BODY = 'body'
+COMMIT_ID = 'id'
+COMMIT_AUTHOR_NAME = 'author_name'
+COMMIT_AUTHOR_EMAIL = 'author_email'
+COMMIT_DATE = 'date'
+COMMIT_MESSAGE = 'message'
+COMMIT_CHANGE_ID = 'change_id'
 
-    print(str)
-    git_command = 'git log ' + str + ' --format="%H%x1f%an%x1f%ae%x1f%ad%x1f%s%x1f%b%x1e"'
-    print(git_command)
-    p = subprocess.Popen(git_command, shell=True, stdout=subprocess.PIPE)
-    (log, _) = p.communicate()
-    log = log.decode(encoding='UTF-8')
-    log = log.strip('\x1e').split("\x1e")
-    log = [row.strip().split("\x1f") for row in log]
-    log = [dict(zip(GIT_COMMIT_FIELDS, row)) for row in log]
 
-    # print(log)
+def git_log(branch_name):
+    git_commit_fields = [COMMIT_ID, COMMIT_AUTHOR_NAME, COMMIT_AUTHOR_EMAIL, COMMIT_DATE, COMMIT_MESSAGE, COMMIT_BODY]
+    git_log_command = 'git --no-pager log ' + branch_name + ' --format="%H%x1f%an%x1f%ae%x1f%ad%x1f%s%x1f%b%x1e"'
+    print(git_log_command)
+    p = subprocess.Popen(git_log_command, shell=True, stdout=subprocess.PIPE)
+    (logs, _) = p.communicate()
+    logs = logs.decode(encoding='UTF-8')
+    logs = logs.strip('\x1e').split("\x1e")
+    logs = [log_str.strip().split("\x1f") for log_str in logs]
+    logs = [dict(zip(git_commit_fields, log_str)) for log_str in logs]
 
-    for row in log:
-        if row.setdefault('body', "NONE").find("\n"):
-            changeIdSet = False
-            for line in row['body'].split("\n"):
+    for one_log in logs:
+        if one_log.setdefault(COMMIT_BODY, "NONE").find("\n"):
+            change_id_set = False
+            for line in one_log[COMMIT_BODY].split("\n"):
                 if line.find('Change-Id:') != -1:
-                    changeIdStartStr = line.split('Change-Id: ')[1].strip()
-                    row['changeId'] = changeIdStartStr.split('\n')[0].strip()
-                    changeIdSet = True
+                    change_id_start_str = line.split('Change-Id: ')[1].strip()
+                    one_log[COMMIT_CHANGE_ID] = change_id_start_str.split('\n')[0].strip()
+                    change_id_set = True
                     break
-            if not changeIdSet:
-                row['changeId'] = "NOT_FIND"
+            if not change_id_set:
+                one_log[COMMIT_CHANGE_ID] = "NOT_FIND"
         else:
-            row['changeId'] = "NONE"
+            one_log[COMMIT_CHANGE_ID] = "NONE"
 
     # for row in log:
-    #     print(row['id'] + " - " + row.get('changeId'))
-    return log
+    #     print(row[COMMIT_ID] + " - " + row.get(COMMIT_CHANGE_ID))
+    return logs
+
 
 log1 = git_log(sys.argv[1])
 changeId1 = []
 for row in log1:
-    if row.get('changeId') != 'NOT_FIND':
-        changeId1.append(row.get('changeId'))
-
-print(len(changeId1))
+    if row.get(COMMIT_CHANGE_ID) != 'NOT_FIND':
+        changeId1.append(row.get(COMMIT_CHANGE_ID))
 
 log2 = git_log(sys.argv[2])
 changeId2 = []
 for row in log2:
-    if row.get('changeId') != 'NOT_FIND':
-        changeId2.append(row.get('changeId'))
+    if row.get(COMMIT_CHANGE_ID) != 'NOT_FIND':
+        changeId2.append(row.get(COMMIT_CHANGE_ID))
 print(len(changeId2))
 
 diff_list = list(set(changeId1).difference(set(changeId2)))
@@ -58,5 +60,5 @@ diff_list = list(set(changeId1).difference(set(changeId2)))
 print("                               commit id" + " - " + "changeId                        ")
 for diff in diff_list:
     for row in log1:
-        if row.get('changeId') == diff:
-            print(row['id'] + " - " + row['changeId'])
+        if row.get(COMMIT_CHANGE_ID) == diff:
+            print(row[COMMIT_ID] + " - " + row[COMMIT_CHANGE_ID])
